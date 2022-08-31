c9c665367912baff4697b37a9314bfcfbfa15016
Andrien Ricketts <andrien.ricketts@ably.com>
2022-08-31 10:38:22 +0100

    Add image header to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
 
 
 class App extends React.Component {
   render() {
-    return (<div className="App">Quiz React</div>)
+    return (
+      <div className="App">
+        <Header/>
+      </div>
+    )
   }
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,7 @@
+import quiz_header from '../quiz_letters_red.png';
+
+function Header() {
+    return <img src={quiz_header} alt="Quiz Header" />;
+}
+
+export default Header;
